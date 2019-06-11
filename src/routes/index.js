96918a8dfb5449e45bbc185fdaa5f07490af56e6
Allison Irvin <allison.irvin2@gmail.com>
2019-06-11 20:52:50 +1000

    Added register routes and tests

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,7 +3,7 @@ import koaJwt from 'koa-jwt'
 import jwt from '../auth/jwt'
 
 // unsecured router
-export const router = (app) => {
+export const router = async (app) => {
     const router = new KoaRouter()
 
     router.get('/', async (ctx) => {
@@ -11,16 +11,38 @@ export const router = (app) => {
         ctx.body = { message: `Hello ${name}!` }
     })
 
+    router.get('/register', async (ctx) => {
+        ctx.body = { message: "Please enter your details" }
+    })
+
+    router.post('/register', async (ctx) => {
+        const firstname = ctx.request.body.firstname
+        const lastname = ctx.request.body.lastname
+        const email = ctx.request.body.email
+        const hash = ctx.request.body.hash
+
+        const dbUser = await app.users.find({ "email": email }).toArray()
+
+        if (dbUser.length === 0) {
+            app.users.insertOne({ "firstname": firstname, "lastname": lastname, "email": email, "hash": hash })
+            ctx.body = { message: `Email ${email} successfully registered` }
+        } else {
+            ctx.status = 409
+            ctx.body = { error: "Email already registered" }
+        }
+    })
+
     router.get('/login', async (ctx) => {
         ctx.body = { message: "Please enter your username and password" }
-        console.log("enter username and password")
     })
 
     router.post('/login', async (ctx) => {
-        let username = ctx.request.body.username
-        let password = ctx.request.body.password
-
-        if (username === "user" && password === "pwd") {
+        let email = ctx.request.body.email
+        let hash = ctx.request.body.hash
+        const dbUser = await app.users.find({ "email": email }).toArray()
+        console.log(dbUser[0].hash)
+        console.log(hash)
+        if (dbUser[0] && dbUser[0].hash === hash) {
             ctx.body = {
                 token: jwt.issueJwtToken({
                     user: "user",
--- test/test.js
+++ test/test.js
@@ -12,20 +12,56 @@ describe("GET at /", () => {
     })
 })
 
-describe("GET at /login", () => {
+describe("GET at /register", () => {
     it("should return 200", async () => {
-        const response = await axios.get(`${URL}/login`)
+        const response = await axios.get(`${URL}/register`)
         expect(response.status).to.be.equal(200)
     })
 })
 
-describe("POST at /login", () => {
-    it("should return 200 with correct username and password", async () => {
-        const response = await axios.post(`${URL}/login`, {
-            "username": "user",
-            "password": "pwd"
-        },
-        )
+describe("POST at /register", () => {
+    it("should return 200 when unique email used", async () => {
+        const response = await axios.post(`${URL}/register`, {
+            "firstname": "John",
+            "lastname": "Smith",
+            "email": "john9@example.com",
+            "hash": "fdafdafda"
+        })
         expect(response.status).to.be.equal(200)
+    })
+
+    describe("GET at /login", () => {
+        it("should return 200", async () => {
+            const response = await axios.get(`${URL}/login`)
+            expect(response.status).to.be.equal(200)
+        })
+    })
+
+    describe("POST at /login", () => {
+        it("should return 200 with correct username and password", async () => {
+            try {
+                const response = await axios.post(`${URL}/login`, {
+                    "email": "john@example.com",
+                    "hash": "fdafdafda"
+                })
+                expect(response.status).to.be.equal(200)
+            } catch (err) {
+                expect(err).to.be.equal(false)
+            }
+        })
+    })
+
+    describe("POST at /login", () => {
+        it("should return 200 with correct username and password", async () => {
+            try {
+                const response = await axios.post(`${URL}/login`, {
+                    "email": "john@example.com",
+                    "hash": "wrong password"
+                })
+                expect(response.status).to.be.equal("Should have thrown error")
+            } catch (err) {
+                expect(err.response.status).to.be.equal(401)
+            }
+        })
     })
 })
